14920ff3d19fdfa5bd6e94cb46a3bdc18ca4b63f
Сергей Захаров <sergei86zahar@mail.ru>
2020-12-16 11:32:50 +0700

    1

--- JS/Hello.js
+++ JS/Hello.js
@@ -1,11 +1,15 @@
-alert( 'Привет, мир!' );
+alert('Привет, мир!');
 let age = prompt('Сколько тебе лет?', 100);
 alert(`Тебе ${age} лет!`); // Тебе 100 лет!
 let isBoss = confirm("Ты здесь главный?");
-alert( isBoss ); // true, если нажата OK
+alert(isBoss); //     true, если нажата OK
 function showMessage() {
-    alert( 'Всем привет!' );
+    alert('Всем привет!');
 }
 
 showMessage();
 showMessage();
+
+for (let i = 0; i < 5; i++) {
+    console.log("value,", i);
+}
